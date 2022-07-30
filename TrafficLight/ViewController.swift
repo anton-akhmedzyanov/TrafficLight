11ced5ba342ae349046e7bf43fcf4f79d4c41387
Anton Akhmedzyanov <anton.akhmedzyanov@gmail.com>
2022-07-30 19:57:38 +0300

    hw # 2 trafficLights

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    var counter = 0
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewWillLayoutSubviews()
+        startButton.layer.cornerRadius = 10
+        
+        
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+        
+        
+        redView.alpha = 0.2
+        yellowView.alpha = 0.2
+        greenView.alpha = 0.2
+            }
+
+    @IBAction func pressStar() {
+        startButton.setTitle(redView.alpha == 0.2 ? "Start" : "Next", for: .normal)
+        
+        counter += 1
+        switch counter {
+        case 1:
+            redView.alpha = 1
+        case 2:
+            redView.alpha = 0.2
+            yellowView.alpha = 1
+        case 3:
+            yellowView.alpha = 0.2
+            greenView.alpha = 1
+        default:
+            greenView.alpha = 0.2
+            counter = 0
+            }
+        
+        
+        
+        
     }
-
-
 }
 
